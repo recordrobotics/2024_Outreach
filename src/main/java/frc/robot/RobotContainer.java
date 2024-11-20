84e0da66fd646b441f21332443e5bf2d6f9664a1
Isaac <barkerisaacd@gmail.com>
2024-11-20 17:43:50 -0500

    import shenanigans

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,7 +3,6 @@ package frc.robot;
 // WPILib imports
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 // Local imports
 import frc.robot.commands.manual.*;
